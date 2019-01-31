9628ea86dc5ab3e0ba820053816c19ca9858e0c6
bergice <andre.kiste@silverstripe.com>
2019-01-31 17:19:49 +1300

    Just reset the whole table

--- app/src/migrate_files/DummyFileTask.php
+++ app/src/migrate_files/DummyFileTask.php
@@ -44,10 +44,40 @@ SQL;
             $parameters []= $i;
         }
 
-        DB::Query("DELETE IGNORE FROM `File`");
-        if (!array_key_exists('Filename', DB::field_list('File'))) {
-            DB::create_field('File', 'Filename', 'mediumtext AFTER `Title`');
-        }
+        DB::query("DROP TABLE IF EXISTS `File`");
+        DB::query(<<<SQL
+CREATE TABLE `File` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ClassName` enum('SilverStripe\\Assets\\File','SilverStripe\\Assets\\Folder','SilverStripe\\Assets\\Image') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT 'SilverStripe\\Assets\\File',
+  `LastEdited` datetime DEFAULT NULL,
+  `Created` datetime DEFAULT NULL,
+  `Name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Filename` mediumtext,
+  `Content` mediumtext,
+  `ShowInSearch` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `ParentID` int(11) NOT NULL DEFAULT '0',
+  `OwnerID` int(11) NOT NULL DEFAULT '0',
+  `Version` int(11) NOT NULL DEFAULT '0',
+  `CanViewType` enum('Anyone','LoggedInUsers','OnlyTheseUsers','Inherit') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT 'Inherit',
+  `CanEditType` enum('LoggedInUsers','OnlyTheseUsers','Inherit') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT 'Inherit',
+  `FileHash` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `FileFilename` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `FileVariant` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `CompanyID` int(11) NOT NULL DEFAULT '0',
+  `BasicFieldsTestPageID` int(11) NOT NULL DEFAULT '0',
+  `TestPageID` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`),
+  KEY `ParentID` (`ParentID`),
+  KEY `OwnerID` (`OwnerID`),
+  KEY `Name` (`Name`(191)),
+  KEY `ClassName` (`ClassName`),
+  KEY `CompanyID` (`CompanyID`),
+  KEY `BasicFieldsTestPageID` (`BasicFieldsTestPageID`),
+  KEY `TestPageID` (`TestPageID`)
+) ENGINE=InnoDB AUTO_INCREMENT=1002 DEFAULT CHARSET=utf8;
+SQL
+        );
         if (!empty($values)) {
             DB::prepared_query('INSERT INTO `File` VALUES ' . implode(',', $values), $parameters);
         }
