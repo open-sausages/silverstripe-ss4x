3321e9ba3420528681705753f2e4a08ae9d3b9e6
Maxime Rainville <maxime@silverstripe.com>
2019-08-21 16:11:58 +1200

    Demo the new insert block here feature

--- app/src/Page.php
+++ app/src/Page.php
@@ -2,11 +2,14 @@
 
 namespace {
 
+    use DNADesign\Elemental\Extensions\ElementalPageExtension;
     use SilverStripe\CMS\Model\SiteTree;
 
     class Page extends SiteTree
     {
         private static $db = [];
+
+        private static $extensions = [ElementalPageExtension::class];
 
         private static $has_one = [];
     }
